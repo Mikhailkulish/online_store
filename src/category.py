d34b52749414315b06bda2a3e5c41a5561a926e7
mikhail kulish <mkulis704@gmail.com>
2026-06-04 14:14:11 +0300

    add category.py

--- src/category.py
+++ src/category.py
@@ -4,8 +4,22 @@ class Category():
     description: str
     products: list
 
+    total_categories = 0
+    total_products = 0
+
     def __init__(self, name, description, products=None):
-        """Инициализауия атрибутов объектов класса категорий"""
-        self.name = name,
-        self.description = description,
+        """Инициализация атрибутов объектов класса категорий"""
+        self.name = name
+        self.description = description
         self.products = products if products else []
+
+        Category.total_categories += 1
+        Category.total_products += len(self.products)
+
+    @property
+    def category_count(self):
+        return Category.total_categories
+
+    @property
+    def product_count(self):
+        return Category.total_products
--- src/product.py
+++ src/product.py
@@ -3,11 +3,11 @@ class Product():
     name: str
     description: str
     price: float
-    quanity: int
+    quantity: int
 
-    def __init__(self, name, description, price, quanity):
+    def __init__(self, name, description, price, quantity):
         """Инициализауия атрибутов объектов класса продуктов"""
-        self.name = name,
-        self.description = description,
-        self.price = price,
-        self.quanity = quanity
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
