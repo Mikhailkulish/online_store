4f3c1bba84f6e81f578c8b53eee0f839811a9cac
mikhail kulish <mkulis704@gmail.com>
2026-06-04 13:29:58 +0300

    add product.py

---
+++ src/product.py
@@ -0,0 +1,11 @@
+class Product():
+    name: str
+    description: str
+    price: float
+    quanity: int
+
+    def __init__(self, name, description, price, quanity):
+        self.name = "name",
+        self.description = "description",
+        self.price = price,
+        self.quanity = quanity
