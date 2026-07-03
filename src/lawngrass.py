62bc15f14371af23b5bc6de5128a51e4c2904fa6
Михаил Кулиш <mkulis704@gmal.com>
2026-07-03 10:56:24 +0300

    testing

--- src/lawngrass.py
+++ src/lawngrass.py
@@ -2,11 +2,11 @@ from src.product import Product
 
 
 class LawnGrass(Product):
-    """Класс газонокосилок"""
+    """Класс газонной травы"""
 
-    def __init__(self, name, description, __price, quantity, country, germination_period, color):
-        """Инициализация атрибутов объектов класса газонокосилок"""
-        super().__init__(name, description, __price, quantity, country, germination_period, color)
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        """Инициализация атрибутов объектов класса газонной травы"""
+        super().__init__(name, description, price, quantity, country, germination_period, color)
         self.country = country
         self.germination_period = germination_period
         self.color = color
--- src/smartphone.py
+++ src/smartphone.py
@@ -4,9 +4,9 @@ from src.product import Product
 class Smartphone(Product):
     """Класс смартфонов"""
 
-    def __init__(self, name, description, __price, quantity, efficiency, model, memory, color):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         """Инициализация атрибутов объектов класса смартфонов"""
-        super().__init__(name, description, __price, quantity, efficiency, model, memory, color)
+        super().__init__(name, description, price, quantity, efficiency, model, memory, color)
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
